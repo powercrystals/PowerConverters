f50326308f1a6274bde6215bfb5c26e28725ccb4
powercrystals <powcrystals@gmail.com>
2013-02-17 20:37:37 -0500

    Promoting build to 2.2.1

--- src/powercrystals/powerconverters/PowerConverterCore.java
+++ src/powercrystals/powerconverters/PowerConverterCore.java
@@ -69,7 +69,7 @@ public class PowerConverterCore implements IUpdateableMod
 {
 	public static final String modId = "PowerConverters";
 	public static final String modName = "Power Converters";
-	public static final String version = "1.4.6R2.2.1B1";
+	public static final String version = "1.4.6R2.2.1";
 	
 	@SidedProxy(clientSide="powercrystals.powerconverters.net.ProxyClient", serverSide="powercrystals.powerconverters.net.ProxyServer")
 	public static IPCProxy proxy;
